87bbe73add37c390738b07e7841c0e0e9c6ac59c
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-24 14:24:03 -0700

    Implement wait-travis

---
+++ src/wait_travis/api.py
@@ -0,0 +1,68 @@
+import subprocess
+from urllib.parse import urlparse
+
+import requests
+
+TOKENS = {}
+
+
+def get_token(endpoint):
+    try:
+        return TOKENS[endpoint]
+    except KeyError:
+        pass
+
+    if "//api.travis-ci.com" in endpoint:
+        clioptions = ["--com"]
+    elif "//api.travis-ci.org" in endpoint:
+        clioptions = ["--org"]
+    else:
+        raise ValueError("Unknown URL: {}".format(endpoint))
+
+    cmd = ["travis", "token"]
+    cmd.extend(clioptions)
+    token = subprocess.check_output(cmd, universal_newlines=True).strip()
+
+    TOKENS[endpoint] = token
+    return token
+
+
+class TravisAPI:
+    @classmethod
+    def com(cls):
+        return cls("https://api.travis-ci.com")
+
+    @classmethod
+    def org(cls):
+        return cls("https://api.travis-ci.org")
+
+    @classmethod
+    def from_url(cls, url):
+        if "//travis-ci.com" in url:
+            return cls.com()
+        elif "//travis-ci.org" in url:
+            return cls.org()
+        raise ValueError("Unknown URL: {}".format(url))
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
+
+    def get_token(self):
+        return get_token(self.endpoint)
+
+    def get_response(self, *path):
+        token = self.get_token()
+        headers = {"Authorization": "token {}".format(token), "Travis-API-Version": "3"}
+        url = "/".join((self.endpoint, *path))
+        return requests.get(url, headers=headers)
+
+    def get(self, *path, **kwargs):
+        return self.get_response(*path, **kwargs).json()
+
+
+def travis_build_id(url):
+    result = urlparse(url)
+    p0, _user, _repo, builds, buildid = result.path.split("/", 4)
+    assert p0 == ""
+    assert builds == "builds"
+    return buildid
--- src/wait_travis/cli.py
+++ src/wait_travis/cli.py
@@ -1,9 +1,57 @@
+import itertools
+import pprint
+import time
+from logging import DEBUG, getLogger
+
 import click
+import click_log
+
+from . import __name__ as root_name
+from .api import TravisAPI, travis_build_id
+
+root_logger = getLogger(root_name)
+click_log.basic_config(root_logger)
+
+logger = getLogger(__name__)
+
+
+def wait_build(api, buildid, interval: float, limit: int):
+    if limit < 0:
+        counter = itertools.count()
+        den = ""
+    else:
+        counter = range(1, 1 + limit)
+        den = "/{}".format(limit)
+
+    for n in counter:
+        logger.info("Polling %d%s...", n, den)
+        build = api.get("build", buildid)
+        logger.debug("Relived state = %r", build["state"])
+        if logger.isEnabledFor(DEBUG):
+            logger.debug("%s", pprint.pformat(build))
+        if build["state"] in ("passed", "failed"):
+            break
+        time.sleep(interval)
+    return build
 
 
 @click.command()
-def main():
-    ...
+@click_log.simple_verbosity_option(root_logger, "--log-level", "--verbosity")
+@click.option("--interval", default=30.0)
+@click.option("--limit", default=1000)
+@click.argument("url")
+@click.pass_context
+def main(ctx, url, **kwargs):
+    api = TravisAPI.from_url(url)
+    buildid = travis_build_id(url)
+    build = wait_build(api, buildid, **kwargs)
+
+    state = build["state"]
+    click.echo("Build result: ", nl=False, err=True)
+    click.echo(click.style(state, fg="green" if state == "passed" else "red"), err=True)
+
+    retcode = int(state != "passed")
+    ctx.exit(retcode)
 
 
 if __name__ == "__main__":
